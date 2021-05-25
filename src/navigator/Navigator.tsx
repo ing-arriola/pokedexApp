7fcdf532453b24661b743d76b7a324e4bf6dc848
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-25 16:27:02 -0600

    created a stack navigator

---
+++ src/navigator/Navigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/HomeScreen';
+import { PokemonScreen } from '../screens/PokemonScreen';
+
+const Stack = createStackNavigator();
+
+const Navigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+      
+    </Stack.Navigator>
+  );
+}
+
+export default Navigator
