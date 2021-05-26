9462dd100438573579e7e8c44f1ce5404d184073
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-26 05:42:14 -0600

    Displayion a list of pokemos using a flatList and customHook

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,30 +1,52 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { FlatList, Text ,Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../components/PokemonCard';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 import { styles } from '../theme/appTheme';
 
 export const HomeScreen = (props) => {
 
     const {top} = useSafeAreaInsets()
 
+    const {pokemonList,getPokemons} = usePokemonPaginated()
+
     return (
-        <View>
+        <>
             <Image 
                 source={require('../assets/pokebola.png')}
                 style={styles.pokebolaBG}
             />
-            <Text
-                style={
-                    {
-                        ...stylesHome.title,
-                        top:top+20,
-                        ...styles.globalMargin
-                    }}
-            >
-                Pokedex
-            </Text>
+            <FlatList 
+                data = { pokemonList } 
+                ListHeaderComponent={(
+                <Text
+                    style={
+                        {
+                            ...stylesHome.title,
+                            top:top+20,
+                            ...styles.globalMargin
+                        }}
+                >
+                    Pokedex
+                </Text>)}
+                showsVerticalScrollIndicator={false}
+                numColumns={2}
+                keyExtractor = {(pokemon) => pokemon.id}
+                renderItem = { ({item}) => (<PokemonCard pokemon={item} />) } 
+                //infinite scroll
+                onEndReached={getPokemons}
+                onEndReachedThreshold={0.4}
+                ListFooterComponent={(
+                    <ActivityIndicator 
+                        style={{height:25}}
+                        size={25}
+                        color='grey'
+                    />
+                )}
+            />
 
-        </View>
+        </>
             
     )
 }
@@ -32,6 +54,7 @@ export const HomeScreen = (props) => {
 const stylesHome= StyleSheet.create({
     title:{
         fontSize:35,
-        fontWeight:'bold'
+        fontWeight:'bold',
+        marginBottom:20
     }
 })
