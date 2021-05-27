9f679b6301fc9f42e77a9727dd270bf8a67769c4
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-27 06:20:43 -0600

    styles for container,title and regular text

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -45,3 +45,19 @@ export const PokemonDetails = ( {pokemon} : Props ) => {
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        marginHorizontal:20
+    },
+    title:{
+        fontSize:22,
+        fontWeight:'bold',
+        marginTop:20
+    },
+    regularText:{
+        fontSize:17,
+        marginRight:10
+    },
+}
+)
