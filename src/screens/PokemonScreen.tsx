6c2d47c73a550cebc9fae0bf4b10b48c2518e8b3
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-27 06:10:01 -0600

    Showing header with pokemon screen, pokeball as bg and navigation button to go back

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -20,10 +20,42 @@ export const PokemonScreen = ( { navigation, route,  }:Props ) => {
     const {pokemon, isLoading} = usePokemon(id)
     
     return (
-        <View>
-            <Text>
-                PokemonScreen
+        
+       <View style={{flex:1}} >
+            <View style={{
+            ...styles.header,
+            backgroundColor:color,
+        }} >
+                <TouchableOpacity
+                    style={{
+                        ...styles.backButton,
+                        top:top + 10
+                    }}
+                    onPress={()=> navigation.goBack()}    
+                >
+                    <Icon
+                        name='arrow-back-outline'
+                        color='#fff'
+                        size={35}
+                    />
+                </TouchableOpacity>
+                <Text 
+                    style={{
+                        ...styles.name,
+                        top:top+40
+                    }}
+                >
+                    {name + '\n' }#{id}
             </Text>
+                <Image
+                    style={styles.whitePokeBall}
+                    source={require('../assets/pokebola-blanca.png')}
+                />
+                <FadeInImage
+                    style={styles.pokemonImage}
+                    uri={picture}
+                />
+            </View>
         </View>
             
     )
