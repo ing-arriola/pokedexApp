d3e9a189a5a1912ff474ea02f62bb07450b70014
ing-arriola <ingeniero.arriola@gmail.com>
2021-05-27 06:04:33 -0600

    Imported custom hook and pokemon detail component

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -5,6 +5,8 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon  from 'react-native-vector-icons/Ionicons';
 import { FadeInImage } from '../components/FadeInImage';
 import { RootStackParams } from '../navigator/Navigator';
+import { usePokemon } from '../hooks/usePokemon';
+import { PokemonDetails } from '../components/PokemonDetails';
 
 interface Props extends StackScreenProps<RootStackParams , 'PokemonScreen' >  {}
 export const PokemonScreen = ( { navigation, route,  }:Props ) => {
